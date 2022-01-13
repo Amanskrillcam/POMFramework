bc2b88dd4987b7cfcab46e29a15102ae2edd33a5
AmanShukla <aman.shukla@infostride.com>
2022-01-13 16:07:16 +0530

    First Selenium project.

--- mavenselenium/src/test/java/First.java
+++ mavenselenium/src/test/java/First.java
@@ -6,7 +6,7 @@ public class First {
             System.setProperty("webdriver.chrome.driver","C:\\Users\\AmanShukla\\Selenium\\chromedriver.exe");
             WebDriver driver=new ChromeDriver();
             driver.manage().window().maximize();
-            driver.get("http://www.yahoo.com");
+            driver.get("http://www.gmail.com");
             System.out.println(driver.getTitle());
             driver.quit();
         }
