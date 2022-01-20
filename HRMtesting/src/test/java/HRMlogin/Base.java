89defa538c8a404d3b447f0ce336c08eda3ea217
AmanShukla <aman.shukla@infostride.com>
2022-01-20 22:04:04 +0530

    calendar automation commit

--- HRMtesting/src/test/java/HRMlogin/Base.java
+++ HRMtesting/src/test/java/HRMlogin/Base.java
@@ -1,13 +1,15 @@
 package HRMlogin;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 class Hrm
@@ -15,26 +17,33 @@ class Hrm
      WebDriver driver = new ChromeDriver();
     void login()
     {
-        driver.get("https://opensource-demo.orangehrmlive.com/");
+        driver.get("http://orangehrm.qedgetech.com/symfony/web/index.php/auth/login");
         driver.manage().window().maximize();
         WebElement a = driver.findElement(By.name("txtUsername"));
         a.sendKeys("Admin");
-        driver.findElement(By.xpath("//*[@id=\"txtPassword\"]")).sendKeys("admin123");
+        driver.findElement(By.xpath("//*[@id=\"txtPassword\"]")).sendKeys("Qedge123!@#");
         driver.findElement(By.id("btnLogin")).click();
 
     }
     void adduser() throws InterruptedException
     {
-        driver.findElement(By.id("menu_pim_viewPimModule")).click();
-        driver.findElement(By.xpath("//*[@id=\"menu_pim_addEmployee\"]")).click();
+        driver.findElement(By.linkText("PIM")).click();
+        driver.findElement(By.id("menu_pim_addEmployee")).click();
         driver.findElement(By.id("firstName")).sendKeys("Aatimiit");
         driver.findElement(By.id("lastName")).sendKeys("verma");
         driver.findElement(By.id("chkLogin")).click();
         driver.findElement(By.id("user_name")).sendKeys("Aatim89");
-        driver.findElement(By.id("user_password")).sendKeys("Amana007");
-        driver.findElement(By.id("re_password")).sendKeys("Amana007");
-        driver.findElement(By.id("btnSave")).click();
+        driver.findElement(By.id("user_password")).sendKeys("QAT@123_team");
+        driver.findElement(By.id("re_password")).sendKeys("QAT@123_team");
+       // driver.findElement(By.id("btnSave")).click();
         Thread.sleep(3000);
+        WebElement element= driver.findElement(By.xpath("//*[@id='btnSave']"));
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", element);
+        String txt = driver.findElement(By.id("successBodyEdit")).getText();
+        Assert.assertEquals(txt,"");
+        System.out.println(txt);
+        System.out.println("User Created successfully......");
 
     }
     void verifyuserisadded()
@@ -50,6 +59,7 @@ class Hrm
        // org.junit.Assert.assertEquals(expectedUsername,actualusername,"User not found");
         Assert.assertEquals(expectedUsername,actualusername,"User not found");*/
 
+
     }
     void Dropdown() throws InterruptedException
     {
@@ -63,9 +73,61 @@ class Hrm
     }
     void calender()
     {
+        String dot="9/October/2018";
+        String date,month,year;
+        String caldt,calmonth,calyear;
+        /*
+         * Split the String into String Array
+         */
+        String dateArray[]= dot.split("/");
+        date=dateArray[0];
+        month=dateArray[1];
+        year=dateArray[2];
         driver.findElement(By.id("menu_recruitment_viewRecruitmentModule")).click();
         driver.findElement(By.id("menu_recruitment_viewCandidates")).click();
-        //dates from
+        WebElement cal;
+        cal=driver.findElement(By.className("calendar"));
+        calyear=driver.findElement(By.className("ui-datepicker-year")).getText();
+
+
+        while (!calyear.equals(year))
+        {
+            driver.findElement(By.className("nextMonth")).click();
+            calyear=driver.findElement(By.className("ui-datepicker-year")).getText();
+            System.out.println("Displayed Year::" + calyear);
+        }
+
+        calmonth=driver.findElement(By.className("ui-datepicker-month")).getText();
+
+        while (!calmonth.equalsIgnoreCase(month))
+        {
+            driver.findElement(By.className("nextMonth ")).click();
+            calmonth=driver.findElement(By.className("ui-datepicker-month")).getText();
+        }
+
+        cal=driver.findElement(By.className("calendar"));
+
+        List<WebElement> rows,cols;
+        rows=cal.findElements(By.tagName("tr"));
+        for (int i = 1; i < rows.size(); i++)
+        {
+            cols=rows.get(i).findElements(By.tagName("td"));
+            for (int j = 0; j < cols.size(); j++)
+            {
+                caldt=cols.get(j).getText();
+                if (caldt.equals(date))
+                {
+                    cols.get(j).click();
+                    break;
+                }
+            }
+        }
+
+    }
+
+
+
+       /* //dates from
         WebElement w=driver.findElement(By.id("candidateSearch_fromDate"));
         w.click();
         w.clear();
@@ -77,7 +139,7 @@ class Hrm
         driver.findElement(By.className("ui-datepicker-year"));
         s.selectByValue("2022");
         driver.findElement(By.linkText("19")).click();
-        dateBox.sendKeys(Keys.TAB);*/
+        dateBox.sendKeys(Keys.TAB);
         //dates to
         driver.findElement(By.id("candidateSearch_toDate")).sendKeys("2022-01-21");
         driver.findElement(By.id("btnSrch")).click();
@@ -88,7 +150,7 @@ class Hrm
         driver.findElement(By.linkText("20")).click();
         dateBox.sendKeys("2022-01-21");*/
 
-    }
+
     void edituser()
     {
         driver.findElement(By.linkText("Aatim89723")).click();
@@ -136,12 +198,12 @@ public class Base extends Hrm
         h.login();
         h.adduser();
         h.verifyuserisadded();
-        h.edituser();
-        h.deleteuser();
-        h.Dropdown();
-        h.calender();
-        h.logout();
-        h.close();
+       // h.edituser();
+       // h.deleteuser();
+       // h.Dropdown();
+       // h.calender();
+       // h.logout();
+       // h.close();
     }
 }
 
