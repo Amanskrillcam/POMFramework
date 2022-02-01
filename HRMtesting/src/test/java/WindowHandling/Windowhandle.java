366f7643e4c422b7c47a04fb9edd439cbec72094
AmanShukla <aman.shukla@infostride.com>
2022-02-01 18:19:33 +0530

    window handling

--- HRMtesting/src/test/java/WindowHandling/Windowhandle.java
+++ HRMtesting/src/test/java/WindowHandling/Windowhandle.java
@@ -4,14 +4,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Iterator;
 import java.util.Set;
 
-public class Windowhandle {
+public class Windowhandle
+{
     String driverpath;
     WebDriver driver;
 
 
-    public void Launch() throws InterruptedException {
+    public void Launch() throws InterruptedException
+    {
         //launch using webdriver manager.
         //WebDriverManager.chromedriver().setup();
         driverpath = System.getProperty("user.dir") + "//driver//chromedriver.exe";
@@ -23,15 +26,30 @@ public class Windowhandle {
         //Step3
         driver.manage().window().maximize();
         //Step4
-        driver.get("https://the-internet.herokuapp.com/");
-        driver.findElement(By.linkText("Multiple Windows")).click();
+        driver.get("https://the-internet.herokuapp.com/windows");
+        driver.findElement(By.linkText("Click Here")).click();
         //handling multiple windows
-        String handle=driver.getWindowHandle();
-        Set<String> handles=driver.getWindowHandles();
-        driver.switchTo().window(handle);
+        Set<String> handle=driver.getWindowHandles();
+        System.out.println("ID is:"+handle);
+        Iterator<String> it=handle.iterator();
+        String parent_id= it.next();
+        String child_id=it.next();
+        System.out.println("parent id"+parent_id);
+        System.out.println("child id"+child_id);
+        driver.switchTo().window(child_id);
+        System.out.println(driver.getTitle());
+//         handles=driver.getWindowHandles();
+//        driver.switchTo().window(handle);
+//
+//        //switch to newly created window
+//        String curwindow=driver.getWindowHandle();
 
-        //switch to newly created window
-        String curwindow=driver.getWindowHandle();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Windowhandle oo=new Windowhandle();
+        oo.Launch();
+
 
     }
 }
